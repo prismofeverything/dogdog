d47f4c28efa86c29b45f2854f93b20d21c0750fb
ryan spangler <ryan.spangler@gmail.com>
2013-06-27 22:20:38 -0700

    gort responding to dogdog

--- src/gort/core.clj
+++ src/gort/core.clj
@@ -3,3 +3,12 @@
             [clojure.java.io :as io]
             [irclj.core :as irclj]))
 
+(defn respond-sensibly
+  [irc m]
+  (let [text (:text m)]
+    (if (re-find #"dogdog" text)
+      (irclj/message irc "#instrument" "DOGDOG LOVES YOU"))))
+
+(defn init
+  [irc]
+  (dosync (alter irc assoc-in [:callbacks :privmsg] respond-sensibly)))
