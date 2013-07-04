6371bbf3a7574930e3c0365cca8d663d86d74999
Kyle Dawkins <kdawkins@fastmail.fm>
2013-07-03 22:36:01 -0700

    Add code to allow dogdog to increase its vocab when instructed to.

--- src/gort/twp.clj
+++ src/gort/twp.clj
@@ -1,5 +1,6 @@
 (ns gort.twp
-  (:require [clojure.java.jdbc :as jdbc]))
+  (:require [clojure.java.jdbc :as jdbc]
+            [clojure.string :as string]))
 
 ;; make this configurable
 (def +db-path+  "resources/words.db")
@@ -25,11 +26,26 @@
   (jdbc/with-connection +db-specs+
     (jdbc/insert-records t {:word word})))
 
-(defn three-word-poem []
-  (let [rv (rand 100)
-        words (if (< 33 rv)
-                [(random-adjective) (random-noun) (random-noun)]
-                (if (< 67 rv)
-                  [(random-adjective) (random-adjective) (random-noun)]
-                  [(random-noun) (random-noun) (random-noun)]))]
-        (str (first words) " " (second words) " " (nth words 2))))
+(defn three-word-poem
+  ([] (three-word-poem {}))
+  ([options]
+    (let [adjectives (concat
+                      (remove nil? [(:adjective options)])
+                      (take 3 (repeatedly random-adjective)))
+          nouns (concat
+                 (remove nil? [(:noun options)])
+                 (take 3 (repeatedly random-noun)))
+          adjective-count (min 2 (+ (int (rand 3))
+                                    (if (:adjective options) 1 0)))
+          noun-count (- 3 adjective-count)
+          words (concat (take adjective-count adjectives) (take noun-count nouns))]
+      (string/join " " words))))
+
+;;(defn three-word-poem []
+;;  (let [rv (rand 100)
+;;        words (if (< 33 rv)
+;;                [(random-adjective) (random-noun) (random-noun)]
+;;                (if (< 67 rv)
+;;                  [(random-adjective) (random-adjective) (random-noun)]
+;;                  [(random-noun) (random-noun) (random-noun)]))]
+;;        (str (first words) " " (second words) " " (nth words 2))))
