f92d911557932c0a8189f6033a205176c60daa0a
ryan spangler <ryan.spangler@gmail.com>
2014-02-08 22:06:24 -0800

    latest markov ridiculously more efficient than previous markov

--- project.clj
+++ project.clj
@@ -8,6 +8,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/java.jdbc "0.3.0-alpha4"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
-                 [markov "0.0.15"]
+                 [markov "0.0.17"]
                  [zalgo "11.6.13"]
                  [irclj "0.5.0-alpha2"]])
