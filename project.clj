6cb63373f39e83cc327a8da41d9b59431e8500d5
Caribou <caribou@ip-10-252-162-252.us-west-2.compute.internal>
2014-02-09 04:55:50 +0000

    latest words and nicks

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :jvm-opts ["-Xmx1g"]
+  :jvm-opts ["-Xmx2G"]
   :main gort.core
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/java.jdbc "0.3.0-alpha4"]
